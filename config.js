cf862c719b31a8003ccfb30e740904098c41df07
Dime Najdovski <dime_najdovski@yahoo.com>
2018-11-02 11:55:10 +0100

    final

--- config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    geoKey: "cRxoUwcGCmTPSTNYeq0jtVidw9sFQ8wU",
-    weadtherKey: "b0708b0871093b854dca9cbd4f3b334f",
-    placeKey: "AIzaSyBH1TxM0lSIAaPbyl-x8dou7DAGmuS_Lxs"
-  }
-
-module.exports = config;
